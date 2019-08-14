df2035382643d623a30168838f99dd3a0f6ba4a7
Lê Thị Cẩm Nhi <lethicamnhi@Les-MacBook-Air.local>
2019-08-14 22:37:46 +0700

    delete

--- controllers/document_controller.py
+++ controllers/document_controller.py
@@ -1,9 +1,9 @@
-from services import document_service
+# from services import document_service
 
 
-def find(start: int = 0, end: int = 0):
-    return document_service.find(start, end)
+# def find(start: int = 0, end: int = 0):
+#     return document_service.find(start, end)
 
 
-def insert(file_path: str, content: str) -> str:
-    return document_service.insert(file_path, content)
+# def insert(file_path: str, content: str) -> str:
+#     return document_service.insert(file_path, content)
--- routers/__init__.py
+++ routers/__init__.py
@@ -1,19 +1,10 @@
 """ routes """
 
-from routers.convert_route import blueprint as convert_api
-from routers.document_route import blueprint as document_api
-from routers.extract_route import blueprint as extract_api
-from routers.service_route import blueprint as service_api
+# from routers.convert_route import blueprint as convert_api
 
 
 def init_routers(app):
     """ create routers """
 
     # retrieve data from the database
-    app.register_blueprint(document_api, url_prefix='/api/data')
-
-    # processing files
-    app.register_blueprint(extract_api, url_prefix='/api/extract')
-    app.register_blueprint(convert_api, url_prefix='/api/convert')
-
-    app.register_blueprint(service_api, url_prefix='/api/service')
+    # app.register_blueprint(document_api, url_prefix='/api/data')
